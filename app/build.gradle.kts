13897fa4355c660545d2c722e817fcd97cebe9f8
Javi Martín <javiyt@users.noreply.github.com>
2025-03-26 15:05:23 +0100

    closes #12 Allow minimify (#17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
