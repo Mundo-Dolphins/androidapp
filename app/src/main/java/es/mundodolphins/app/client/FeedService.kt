68e9e9e1cd82e12b90ac794a49324a4cb42fd66b
Javi Martín <javiyt@users.noreply.github.com>
2025-03-26 17:07:13 +0100

    closes #23 Read new endpoint to show the latests episodes (#26)

--- app/src/main/java/es/mundodolphins/app/client/FeedService.kt
+++ app/src/main/java/es/mundodolphins/app/client/FeedService.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface FeedService {
-    @GET("season_7.json")
+    @GET("latests.json")
     suspend fun getFeed(): Response<List<Episode>>
 }
