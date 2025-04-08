6e7cfc2602b862d61ccd1782cdcfb945d834c7b4
Javi Martín <javiyt@users.noreply.github.com>
2025-04-08 17:22:46 +0200

    closes #38 New information row for every episode in the list (#39)

--- app/src/main/java/es/mundodolphins/app/ui/theme/Color.kt
+++ app/src/main/java/es/mundodolphins/app/ui/theme/Color.kt
@@ -5,3 +5,7 @@ import androidx.compose.ui.graphics.Color
 val aqua = Color(0xFF008E97)
 val orange = Color(0xFFFC4C02)
 val blue = Color(0xFF005778)
+val lightGreen = Color(0xFFE2FCEC)
+val darkGreen = Color(0xFF157543)
+val lightYellow = Color(0xFFFEF9CE)
+val darkYellow = Color(0xFF9B6518)
