ee5c583728a5b76234fa190e85779032390abf3a
Javi Martin <me@javi.yt>
2025-04-08 17:28:56 +0200

    closes #40 New session list

--- app/src/main/java/es/mundodolphins/app/ui/views/seasons/SeasonsList.kt
+++ app/src/main/java/es/mundodolphins/app/ui/views/seasons/SeasonsList.kt
@@ -2,6 +2,7 @@ package es.mundodolphins.app.ui.views.seasons
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -14,11 +15,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import es.mundodolphins.app.R
 import es.mundodolphins.app.ui.Routes
+import es.mundodolphins.app.ui.theme.MundoDolphinsTheme
 import es.mundodolphins.app.viewmodel.EpisodesViewModel
 
 @Composable
@@ -58,10 +63,18 @@ fun SeasonsList(
 @Composable
 fun SeasonRow(seasonId: Int, navController: NavController) {
     if (seasonId > 0) {
-        Button(onClick = {
-            navController.navigate(Routes.SeasonView.route + "/$seasonId")
-        }) {
-            Text(text = stringResource(R.string.season, seasonId))
+        Button(
+            modifier = Modifier.fillMaxWidth(0.7f).padding(6.dp),
+            onClick = {
+                navController.navigate(Routes.SeasonView.route + "/$seasonId")
+            }
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = stringResource(R.string.season, seasonId),
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
         }
     }
 }
@@ -69,8 +82,10 @@ fun SeasonRow(seasonId: Int, navController: NavController) {
 @Preview
 @Composable
 fun SeasonsListPreview() {
-    SeasonsList(
-        listOf(7, 6, 5, 4, 3, 2, 1),
-        navController = NavController(LocalContext.current)
-    )
+    MundoDolphinsTheme {
+        SeasonsList(
+            listOf(8, 7, 6, 5, 4, 3, 2, 1),
+            navController = NavController(LocalContext.current)
+        )
+    }
 }
