3a1688663a50abb389b0c3b3809870c862a4df0c
Javi Martin <me@javi.yt>
2025-04-08 21:19:37 +0200

    closes #47 Clean up unused resources

--- app/src/main/java/es/mundodolphins/app/ui/theme/Color.kt
+++ app/src/main/java/es/mundodolphins/app/ui/theme/Color.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val aqua = Color(0xFF008E97)
 val orange = Color(0xFFFC4C02)
-val blue = Color(0xFF005778)
 val lightGreen = Color(0xFFE2FCEC)
 val darkGreen = Color(0xFF157543)
 val lightYellow = Color(0xFFFEF9CE)
