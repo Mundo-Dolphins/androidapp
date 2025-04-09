8e141e6dac304e196a45e517c1c46a0405b5d3c3
Javi Martín <javiyt@users.noreply.github.com>
2025-04-09 10:26:54 +0200

    closes #50 Improve no internet connection error (#53)

--- app/src/main/java/es/mundodolphins/app/MainActivity.kt
+++ app/src/main/java/es/mundodolphins/app/MainActivity.kt
@@ -6,14 +6,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import es.mundodolphins.app.data.AppDatabase
+import es.mundodolphins.app.observer.ConnectivityObserver
 import es.mundodolphins.app.repository.EpisodeRepository
 import es.mundodolphins.app.ui.bar.AppBar
 import es.mundodolphins.app.ui.bar.AppBottomBar
@@ -29,30 +45,68 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val navController = rememberNavController()
-            val viewModel: EpisodesViewModel = viewModel(
-                factory = EpisodesViewModelFactory(
-                    EpisodeRepository(
-                        AppDatabase.getDatabase(context = LocalContext.current.applicationContext)
-                            .episodeDao()
-                    )
-                )
-            )
-
             MundoDolphinsTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    topBar = { AppBar() },
-                    bottomBar = { AppBottomBar(navController) }
-                ) { innerPadding ->
-                    viewModel.refreshDatabase()
-                    MainScreen(
-                        model = viewModel,
-                        modifier = Modifier.padding(innerPadding),
-                        navController = navController
+                MundoDolphinsScreen()
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+fun MundoDolphinsScreen() {
+    val navController = rememberNavController()
+    val viewModel: EpisodesViewModel = viewModel(
+        factory = EpisodesViewModelFactory(
+            EpisodeRepository(
+                AppDatabase.getDatabase(context = LocalContext.current.applicationContext)
+                    .episodeDao()
+            )
+        )
+    )
+    val context = LocalContext.current
+    val connectivityObserver = remember { ConnectivityObserver(context) }
+    val isConnected by connectivityObserver.isConnected.observeAsState(initial = true)
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { AppBar() },
+        bottomBar = { AppBottomBar(navController) }
+    ) { innerPadding ->
+        viewModel.refreshDatabase()
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            if (!isConnected) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .background(color = Color.Red)
+                        .padding(top = 60.dp)
+                ) {
+                    Text(
+                        text = "No internet connection",
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.align(Alignment.Center),
+                        color = Color.Yellow
                     )
                 }
             }
+            MainScreen(
+                model = viewModel,
+                modifier = Modifier.padding(innerPadding),
+                navController = navController
+            )
         }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+@Preview(showBackground = true)
+fun MundoDolphinsScreenPreview() {
+    MundoDolphinsTheme {
+        MundoDolphinsScreen()
     }
 }
--- app/src/main/java/es/mundodolphins/app/ui/views/main/MainNavigation.kt
+++ app/src/main/java/es/mundodolphins/app/ui/views/main/MainNavigation.kt
@@ -4,21 +4,13 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import es.mundodolphins.app.observer.ConnectivityObserver
 import es.mundodolphins.app.ui.Routes
 import es.mundodolphins.app.ui.views.info.EpisodeScreen
 import es.mundodolphins.app.ui.views.links.UsefulLinksScreen
@@ -34,56 +26,44 @@ fun MainScreen(
     model: EpisodesViewModel = viewModel(),
     navController: NavHostController
 ) {
-    val context = LocalContext.current
-    val connectivityObserver = remember { ConnectivityObserver(context) }
-    val isConnected by connectivityObserver.isConnected.observeAsState(initial = true)
-
     Box(modifier = Modifier.fillMaxSize()) {
-        if (isConnected) {
-            NavHost(
-                navController = navController,
-                startDestination = Routes.Feed.route
-            ) {
-                composable(route = Routes.Feed.route) {
-                    EpisodesScreen(
-                        navController = navController,
-                        modifier = modifier,
-                        model = model
-                    )
-                }
-                composable(route = Routes.EpisodeView.route + "/{id}") { backStackEntry ->
-                    model.getEpisode(backStackEntry.arguments?.getString("id")?.toLong() ?: 0)
-                    EpisodeScreen(
-                        episode = model.episode.collectAsState(null).value,
-                        navController = navController,
-                        modifier = modifier
-                    )
-                }
-                composable(route = Routes.UsefulLinks.route) {
-                    UsefulLinksScreen(modifier = modifier)
-                }
-                composable(route = Routes.SeasonsList.route) {
-                    SeasonsListScreen(
-                        modifier = modifier,
-                        navController = navController,
-                        model = model
-                    )
-                }
-                composable(route = Routes.SeasonView.route + "/{id}") {
-                    SeasonsView(
-                        seasonId = it.arguments?.getString("id")?.toInt() ?: 0,
-                        modifier = modifier,
-                        navController = navController,
-                        model = model
-                    )
-                }
+        NavHost(
+            navController = navController,
+            startDestination = Routes.Feed.route
+        ) {
+            composable(route = Routes.Feed.route) {
+                EpisodesScreen(
+                    navController = navController,
+                    modifier = modifier,
+                    model = model
+                )
             }
-        } else {
-            Text(
-                text = "No internet connection",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.align(Alignment.Center)
-            )
+            composable(route = Routes.EpisodeView.route + "/{id}") { backStackEntry ->
+                model.getEpisode(backStackEntry.arguments?.getString("id")?.toLong() ?: 0)
+                EpisodeScreen(
+                    episode = model.episode.collectAsState(null).value,
+                    navController = navController,
+                    modifier = modifier
+                )
+            }
+            composable(route = Routes.UsefulLinks.route) {
+                UsefulLinksScreen(modifier = modifier)
+            }
+            composable(route = Routes.SeasonsList.route) {
+                SeasonsListScreen(
+                    modifier = modifier,
+                    navController = navController,
+                    model = model
+                )
+            }
+            composable(route = Routes.SeasonView.route + "/{id}") {
+                SeasonsView(
+                    seasonId = it.arguments?.getString("id")?.toInt() ?: 0,
+                    modifier = modifier,
+                    navController = navController,
+                    model = model
+                )
+            }
         }
     }
 }
