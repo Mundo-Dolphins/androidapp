8fc2f39506667e171fed483590695580820f7d5e
Javi Martin <me@javi.yt>
2025-04-09 10:28:00 +0200

    Release 1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "es.mundodolphins.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.8"
+        versionCode = 9
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
