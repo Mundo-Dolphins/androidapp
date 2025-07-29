5c34d7fc50a01c0855e9f1be90bdc24a91ae9975
Javi Martin <me@javi.yt>
2025-07-29 10:52:54 +0200

    re #112 Update app to support Android 15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "11"
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
     }
     buildFeatures {
         compose = true
