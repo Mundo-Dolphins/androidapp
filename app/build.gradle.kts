4ad96a336f491362e5edbbea15edafa43ee24c1b
Javi Martin <me@javi.yt>
2026-02-10 20:40:33 +0100

    Release 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "es.mundodolphins.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "2.0.1"
+        versionCode = 12
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
