213dbb87893539d472b531e52e80cb4752675aa2
Javi Martin <me@javi.yt>
2026-05-05 21:19:26 +0200

    update imports on createAndroidComposeRule to v2

--- app/src/test/java/es/mundodolphins/app/ui/videos/VideosScreenTest.kt
+++ app/src/test/java/es/mundodolphins/app/ui/videos/VideosScreenTest.kt
@@ -5,7 +5,7 @@ import android.os.Build
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.google.common.truth.Truth.assertThat
